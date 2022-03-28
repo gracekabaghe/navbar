65c5aed6d84500377433833fa20431a883f96bc5
gracekabaghe <gracema2002@gmail.com>
2022-03-28 15:54:46 +0200

    Add toggle to navbar

---
+++ app.js
@@ -0,0 +1,13 @@
+// select the classes
+const links = document.querySelector(".links");
+const toggle = document.querySelector(".nav-toggle");
+// add event listener
+toggle.addEventListener('click',()=>{
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links')
+    // }else {
+    //     links.classList.add('show-links')
+
+    // }
+    links.classList.toggle('show-links')
+}) 
